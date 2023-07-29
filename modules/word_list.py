7ef46c8a8be61ad7e5a56b49b3d86cf4179aa8fb
djiroff <137865914+djiroff@users.noreply.github.com>
2023-07-29 17:45:35 +0930

    2nd commit

--- modules/word_list.py
+++ modules/word_list.py
@@ -1,12 +1,3 @@
-"""
-Master Computing and Innovation
-MCI-Projects-2023
-Developing and application for cognitive research
-
-Richard Hill (a1756449), Vahid Jahangirov (a1845737), Hanbo Pu (a1807267)
-
-"""
-
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
